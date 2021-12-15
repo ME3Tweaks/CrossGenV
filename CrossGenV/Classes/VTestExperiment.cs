dcc8d9658c9313d2edad33ea4233a4d363ff57c5
Mgamerz <mgamerz2@gmail.com>
2021-12-14 22:28:35 -0700

    Vtest: Add mod global files

--- CrossGenV/Classes/VTestExperiment.cs
+++ CrossGenV/Classes/VTestExperiment.cs
@@ -8,6 +8,7 @@ using System.Text;
 using System.Threading.Tasks;
 using LegendaryExplorerCore.Compression;
 using LegendaryExplorerCore.GameFilesystem;
+using LegendaryExplorerCore.Gammtek.Extensions.Collections.Generic;
 using LegendaryExplorerCore.Helpers;
 using LegendaryExplorerCore.Kismet;
 using LegendaryExplorerCore.Misc;
@@ -1134,6 +1135,8 @@ namespace CrossGenV.Classes
                 using var fs = File.OpenWrite(outpath);
                 stream.CopyTo(fs);
             }
+
+            emptyLevelName = Path.GetFileNameWithoutExtension(emptyLevelName);
             using var Pcc = MEPackageHandler.OpenMEPackage(outpath);
             for (int i = 0; i < Pcc.Names.Count; i++)
             {
@@ -2137,14 +2140,7 @@ namespace CrossGenV.Classes
                 // The original logic is removed in the ModdedSource file
                 #endregion
 
-                #region Mod level files
-                // Step 1: Make more LevelStreamingKismet objects
-
-                // Step 2: Rebuild StreamingLevels
-
-                // Step 3: Add levels to states
-
-                #endregion
+                AddModLevelExtensions(le1File, "BIOA_PRC2");
             }
             else if (fName.CaseInsensitiveEquals("BIOA_PRC2AA"))
             {
@@ -2199,6 +2195,81 @@ namespace CrossGenV.Classes
                 RebuildPersistentLevelChildren(level, vTestOptions);
             }
             //CorrectTriggerStreamsMaybe(me1File, le1File);
+        }
+
+        private static void AddModLevelExtensions(IMEPackage le1File, string levelName)
+        {
+            // Step 1: Make more LevelStreamingKismet objects
+            var levelnames = new List<string>();
+            var sourceToClone = le1File.Exports.First(x => x.ClassName == "LevelStreamingKismet");
+            for (int i = 0; i < 5; i++)
+            {
+                var levelBaseName = levelName + "_ModGlobal" + (i + 1);
+                var newEntry = EntryCloner.CloneEntry(sourceToClone) as ExportEntry;
+                newEntry.WriteProperty(new NameProperty(levelBaseName, "PackageName"));
+                levelnames.Add(levelBaseName);
+            }
+
+            // Step 2: Rebuild StreamingLevels
+            RebuildStreamingLevels(le1File);
+
+            // Step 3: Add levels to states
+            // Doing it via a global didn't seem to work
+            foreach (var bts in le1File.Exports.Where(x => x.ClassName == "BioTriggerStream"))
+            {
+                var streamingStates = bts.GetProperty<ArrayProperty<StructProperty>>("StreamingStates");
+                if (streamingStates != null)
+                {
+                    foreach (var ss in streamingStates)
+                    {
+                        var visibleChunks = ss.GetProp<ArrayProperty<NameProperty>>("VisibleChunkNames");
+                        visibleChunks.AddRange(levelnames.Select(x => new NameProperty(x)));
+                    }
+                }
+                bts.WriteProperty(streamingStates);
+            }
+        }
+
+        private static void RebuildStreamingLevels(IMEPackage Pcc)
+        {
+            try
+            {
+                var levelStreamingKismets = new List<ExportEntry>();
+                ExportEntry bioworldinfo = null;
+                foreach (ExportEntry exp in Pcc.Exports)
+                {
+                    switch (exp.ClassName)
+                    {
+                        case "BioWorldInfo" when exp.ObjectName == "BioWorldInfo":
+                            bioworldinfo = exp;
+                            continue;
+                        case "LevelStreamingKismet" when exp.ObjectName == "LevelStreamingKismet":
+                            levelStreamingKismets.Add(exp);
+                            continue;
+                    }
+                }
+
+                levelStreamingKismets = levelStreamingKismets
+                    .OrderBy(o => o.GetProperty<NameProperty>("PackageName").ToString()).ToList();
+                if (bioworldinfo != null)
+                {
+                    var streamingLevelsProp =
+                        bioworldinfo.GetProperty<ArrayProperty<ObjectProperty>>("StreamingLevels") ??
+                        new ArrayProperty<ObjectProperty>("StreamingLevels");
+
+                    streamingLevelsProp.Clear();
+                    foreach (ExportEntry exp in levelStreamingKismets)
+                    {
+                        streamingLevelsProp.Add(new ObjectProperty(exp.UIndex));
+                    }
+
+                    bioworldinfo.WriteProperty(streamingLevelsProp);
+                }
+            }
+            catch (Exception ex)
+            {
+                Debug.WriteLine("IT DIDN'T WORK!");
+            }
         }
 
         private static void FixLighting(IMEPackage le1File, VTestOptions vTestOptions)
