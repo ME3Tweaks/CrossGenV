bc31d74691056c6de9a0e0603d7fc5654123dd35
Mgamerz <mgamerz@msn.com>
2024-10-15 23:42:43 -0600

    Task 105 - Fix collision to right of cockpit

--- CrossGenV/Classes/Levels/BIOA_PRC2_CCSIM03_LAY.cs
+++ CrossGenV/Classes/Levels/BIOA_PRC2_CCSIM03_LAY.cs
@@ -1,4 +1,5 @@
 ﻿using LegendaryExplorerCore.Packages;
+using LegendaryExplorerCore.Unreal;
 
 namespace CrossGenV.Classes.Levels
 {
@@ -13,6 +14,20 @@ namespace CrossGenV.Classes.Levels
             // garage door
             // The materials changed in LE so using the original set is wrong. Remove this property to prevent porting donors for it
             me1File.FindExport("TheWorld.PersistentLevel.BioDoor_1.SkeletalMeshComponent_1").RemoveProperty("Materials");
+
+            // Task 105 - fix bad collision near ochren by adjusting blocking volumes to prevent player from getting into zipping spots
+            // on the right side of the cockpit seat
+            var bv = me1File.FindExport("TheWorld.PersistentLevel.BlockingVolume_3");
+            bv.WriteProperty(CommonStructs.Vector3Prop(-5336,1184,-1164, "Location"));
+            bv.WriteProperty(CommonStructs.RotatorProp(0, 8192, 0, "Rotation"));
+            bv.WriteProperty(CommonStructs.Vector3Prop(1, 1.7f, 1, "DrawScale3D"));
+
+            // Task 105 - Mirror left side
+            bv = me1File.FindExport("TheWorld.PersistentLevel.BlockingVolume_2");
+            bv.WriteProperty(CommonStructs.Vector3Prop(-5386, 1510, -1164, "Location"));
+            bv.WriteProperty(CommonStructs.RotatorProp(0, -8192, 0, "Rotation"));
+            bv.WriteProperty(CommonStructs.Vector3Prop(1, 2f, 1, "DrawScale3D"));
+
         }
 
         public void PostPortingCorrection()
