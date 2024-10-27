b641756f57e697c627b6e73aef7126a6652cf042
henbagle <ben.hagle@gmail.com>
2024-10-27 16:10:45 -0500

    Add new string refs to VTestTLK

--- CrossGenV/Classes/VTestTLK.cs
+++ CrossGenV/Classes/VTestTLK.cs
@@ -24,49 +24,79 @@ namespace CrossGenV.Classes
             {
                 case "RA":
                 case "RU":
-                    stringRefs.Add(new TLKStringRef(338464, "Загрузка данных"));
-                    stringRefs.Add(new TLKStringRef(338465, "Настройка музыки"));
-                    stringRefs.Add(new TLKStringRef(338466, "Отключить музыку"));
-                    stringRefs.Add(new TLKStringRef(338467, "Включить музыку"));
+                    AddEnglishStringRefs();
                     break;
                 case "ES":
-                    stringRefs.Add(new TLKStringRef(338465, "Configuración de Música"));
-                    stringRefs.Add(new TLKStringRef(338466, "Desactivar Música"));
-                    stringRefs.Add(new TLKStringRef(338467, "Activar Música"));
+                    AddEnglishStringRefs();
                     break;
                 case "IT":
-                    stringRefs.Add(new TLKStringRef(338465, "Ustawienia muzyki"));
-                    stringRefs.Add(new TLKStringRef(338466, "Wyłącz muzykę"));
-                    stringRefs.Add(new TLKStringRef(338467, "Włącz muzykę"));
+                    AddEnglishStringRefs();
                     break;
                 case "PL":
                 case "PLPC":
-                    stringRefs.Add(new TLKStringRef(338465, "Music Setting"));
-                    stringRefs.Add(new TLKStringRef(338466, "Disable Music"));
-                    stringRefs.Add(new TLKStringRef(338467, "Enable Music"));
+                    AddEnglishStringRefs();
                     break;
                 case "FR":
-                    stringRefs.Add(new TLKStringRef(338465, "Music Setting"));
-                    stringRefs.Add(new TLKStringRef(338466, "Disable Music"));
-                    stringRefs.Add(new TLKStringRef(338467, "Enable Music"));
+                    AddEnglishStringRefs();
                     break;
                 case "DE":
-                    stringRefs.Add(new TLKStringRef(338465, "Musikeinstellungen"));
-                    stringRefs.Add(new TLKStringRef(338466, "Musik Deaktivieren"));
-                    stringRefs.Add(new TLKStringRef(338467, "Musik Aktivieren"));
+                    AddEnglishStringRefs();
                     break;
                 case "JA":
-                    stringRefs.Add(new TLKStringRef(338464, "Downloading Data"));
-                    stringRefs.Add(new TLKStringRef(338465, "Music Setting"));
-                    stringRefs.Add(new TLKStringRef(338466, "Disable Music"));
-                    stringRefs.Add(new TLKStringRef(338467, "Enable Music"));
+                    AddEnglishStringRefs();
                     break;
             }
-
-
+            
             var huff = new HuffmanCompression();
             huff.LoadInputData(stringRefs);
             huff.SerializeTalkfileToExport(tlkExport);
+
+            void AddEnglishStringRefs()
+            {
+                stringRefs.Add(new TLKStringRef(338464, "Downloading Data"));
+                stringRefs.Add(new TLKStringRef(338465, "Simulator Settings"));
+                stringRefs.Add(new TLKStringRef(338466, "Open"));
+                stringRefs.Add(new TLKStringRef(338467, "ENABLED"));
+                stringRefs.Add(new TLKStringRef(338468, "DISABLED"));
+                stringRefs.Add(new TLKStringRef(338469, "Enable"));
+                stringRefs.Add(new TLKStringRef(338470, "Disable"));
+                stringRefs.Add(new TLKStringRef(338471, "Music"));
+                stringRefs.Add(new TLKStringRef(338472, "Appropriate music tracks will be added to each simulator map, which will change as the intensity ramps up."));
+                stringRefs.Add(new TLKStringRef(338473, "As in the original DLC, no music will be played in the simulator."));
+                stringRefs.Add(new TLKStringRef(338474, "XP on 1st Place"));
+                stringRefs.Add(new TLKStringRef(338475, "You will be granted experience upon getting 1st place in a simulator map. 1/3 of a level's worth of XP will be granted upon achieving 1st place for the first time on each simulator map. Additional XP will be granted upon beating your record, and upon completing the special scenario."));
+                stringRefs.Add(new TLKStringRef(338476, "No experience will be granted upon completing a simulator map."));
+                stringRefs.Add(new TLKStringRef(338477, "Survival: Enemy Count Ramping"));
+                stringRefs.Add(new TLKStringRef(338478, "Enemy Ramping"));
+                stringRefs.Add(new TLKStringRef(338479, "The amount of enemies that spawn will be fixed to the default, original version."));
+                stringRefs.Add(new TLKStringRef(338480, "The amount of enemies in survival mode will increase over time, creating a increasing difficulty curve as time progresses."));
+                stringRefs.Add(new TLKStringRef(338481, "Difficulty Ramping: Talents"));
+                stringRefs.Add(new TLKStringRef(338482, "As a mission progresses, enemies will gain talents and powers that make them more lethal."));
+                stringRefs.Add(new TLKStringRef(338483, "Enemies will not gain talents as simulator missions progress. This is the default value."));
+                stringRefs.Add(new TLKStringRef(338484, "Difficulty Ramping: Weapons"));
+                stringRefs.Add(new TLKStringRef(338485, "As a mission progresses, enemies will gain weapon mods that make them more lethal."));
+                stringRefs.Add(new TLKStringRef(338486, "Enemies will not gain weapon mods as simulator missions progress. This is the default value."));
+                stringRefs.Add(new TLKStringRef(338487, "Simulator: Enemy Selector"));
+                stringRefs.Add(new TLKStringRef(338488, "Select enemy type"));
+                stringRefs.Add(new TLKStringRef(338489, "This set of enemies is currently not enabled."));
+                stringRefs.Add(new TLKStringRef(338490, "Asari enemies will replace the normal enemies in the combat simulator."));
+                stringRefs.Add(new TLKStringRef(338491, "Salarian enemies will replace the normal enemies in the combat simulator."));
+                stringRefs.Add(new TLKStringRef(338492, "Batarian enemies will replace the normal enemies in the combat simulator."));
+                stringRefs.Add(new TLKStringRef(338493, "Monsters"));
+                stringRefs.Add(new TLKStringRef(338494, "Monsterous enemies will replace the normal enemies in the combat simulator."));
+                stringRefs.Add(new TLKStringRef(338495, "Mixed Enemies"));
+                stringRefs.Add(new TLKStringRef(338496, "Enemies from all available factions will appear in the combat simulator."));
+                stringRefs.Add(new TLKStringRef(338497, "All Settings: Vanilla"));
+                stringRefs.Add(new TLKStringRef(338498, "All Settings"));
+                stringRefs.Add(new TLKStringRef(338499, "All simulator settings will be set to their vanilla, original Mass Effect 1 value. For Pinnacle Station purists."));
+                stringRefs.Add(new TLKStringRef(338500, "All Settings: Recommended"));
+                stringRefs.Add(new TLKStringRef(338501, "All simulator settings will be set to their recommended states for this remaster."));
+                stringRefs.Add(new TLKStringRef(338502, "Selected"));
+                stringRefs.Add(new TLKStringRef(338503, "Not Selected"));
+                stringRefs.Add(new TLKStringRef(338504, "Enemy types will not be changed from the defaults."));
+                stringRefs.Add(new TLKStringRef(338505, "Enemy types have been changed from the defaults."));
+                stringRefs.Add(new TLKStringRef(338506, "Default"));
+            }
         }
 
         public static void PostUpdateTLKs(VTestOptions vTestOptions)
