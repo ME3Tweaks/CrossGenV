1a434d1f5231b838f80e98edbb6ccffb0f9756fa
Mgamerz <mgamerz@msn.com>
2024-10-27 20:10:21 -0600

    Task 97 - Pillar emis fix

--- CrossGenV/Classes/Levels/BIOA_PRC2_CCThai.cs
+++ CrossGenV/Classes/Levels/BIOA_PRC2_CCThai.cs
@@ -1,6 +1,7 @@
 ﻿using LegendaryExplorerCore.Packages;
 using LegendaryExplorerCore.Packages.CloningImportingAndRelinking;
 using LegendaryExplorerCore.Unreal;
+using LegendaryExplorerCore.Unreal.BinaryConverters;
 
 namespace CrossGenV.Classes.Levels
 {
@@ -12,30 +13,17 @@ namespace CrossGenV.Classes.Levels
 
         public void PrePortingCorrection()
         {
-            // Forces different donor
+            // Task 77 - Unset material in ME1/LE1 mesh shows default texture
+            // HenBagle, NaNuke
+            // Forces different donor to be picked up that we made fixed version for
             var matFixObject01 = me1File.FindExport("BIOA_JUG40_S.jug40_Object01");
             matFixObject01.ObjectName = "jug40_Object01_Crossgen"; // NaNuke, HenBagle
 
-            // Changes material to use proper emis // Audemus
-            // First make the proper material that will be converted for donor
-            var preDonor1 = me1File.FindExport("BIOA_PRC2_MatFX.Holodeck_Grid_MAT");
-            var preDonor2 = EntryCloner.CloneEntry(preDonor1);
-            var cgv = ExportCreator.CreatePackageExport(me1File, "CROSSGENV");
-            preDonor2.idxLink = cgv.UIndex;
-            preDonor2.ObjectName = "JUG80_POD01_Crossgen";
-
-
-            var materials = new ArrayProperty<ObjectProperty>("Materials");
-            materials.Add(preDonor2);
+            // Task 97 - Changes material to use proper emis
+            // Audemus
+            // We rename object so it picks up donor with correct material applied. Overrides didn't seem to work
             var pillarMesh = me1File.FindExport("BIOA_JUG40_S.JUG40_PILLARV00");
-            var pillarReferences = pillarMesh.GetEntriesThatReferenceThisOne();
-            foreach (var entry in pillarReferences)
-            {
-                if (entry.Key is ExportEntry exp && exp.ClassName == "StaticMeshComponent")
-                {
-                    exp.WriteProperty(materials);
-                }
-            }
+            pillarMesh.ObjectName = " JUG40_PILLARV00_CROSSGEN";
         }
 
         public void PostPortingCorrection()
