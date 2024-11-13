4948a61f1603a7ad16abd4bf9d12aa4c40f05806
Mgamerz <mgamerz@msn.com>
2024-11-12 20:37:15 -0700

    VTestOptions: Set some fields readonly to indicate they should not be changed.

--- CrossGenV/Classes/VTestOptions.cs
+++ CrossGenV/Classes/VTestOptions.cs
@@ -10,11 +10,13 @@ namespace CrossGenV.Classes
 {
     public class VTestOptions
     {
+        // Readonly options should NOT be changed. They are old legacy development variables!
+
         #region Configurable options
         /// <summary>
         /// List of levels to port. DO NOT INCLUDE BIOA_.
         /// </summary>
-        public string[] vTestLevels = new[]
+        public readonly string[] vTestLevels = new[]
         {
             // Comment/uncomment these to select which files to build. Files commented out will not be included in the DLC folder
             "PRC2",
@@ -24,52 +26,52 @@ namespace CrossGenV.Classes
         /// <summary>
         /// If lightmaps and shadowmaps should be stripped and dynamic lighting turned on
         /// </summary>
-        public bool useDynamicLighting = false;
+        public readonly bool useDynamicLighting = false;
 
         /// <summary>
         /// Strips shadow maps off. If using dynamic lighting, shadow maps are always stripped
         /// </summary>
-        public bool stripShadowMaps = false;
+        public readonly bool stripShadowMaps = false;
 
         /// <summary>
         /// If light and shadowmaps for meshes ported from ME1 (not using LE1 donor) should be ported instead of stripped. This may not look good but may be possible to adjust.
         /// </summary>
-        public bool allowTryingPortedMeshLightMap = true;
+        public readonly bool allowTryingPortedMeshLightMap = true;
 
         /// <summary>
         /// If terrains should have their lightmaps ported over (if they exist)
         /// </summary>
-        public bool portTerrainLightmaps = true;
-
-        /// <summary>
-        /// If the audio localizations should be ported
-        /// </summary>
-        public bool portAudioLocalizations = true;
+        public readonly bool portTerrainLightmaps = true;
 
         /// <summary>
         /// If a level's list of StreamableTextureInstance's should be copied over.
         /// </summary>
-        public bool installTexturesInstanceMap = false;
+        public readonly bool installTexturesInstanceMap = false;
 
         /// <summary>
         /// If a level's list of textures to force streaming should be copied over.
         /// </summary>
-        public bool installForceTextureStreaming = false;
+        public readonly bool installForceTextureStreaming = false;
 
         /// <summary>
         /// The intensity scalar for the CCLava lightmap
         /// </summary>
-        public float LavaLightmapScalar = 0.15f;
+        public readonly float LavaLightmapScalar = 0.15f;
+
+        /// <summary>
+        /// The intensity scalar for the PRC2AA lightmaps when baking lighting
+        /// </summary>
+        public readonly float ApartmentStaticLightScalar = 0.13f;
 
         /// <summary>
         /// If debug features should be enabled in the build
         /// </summary>
-        public bool debugBuild = false;
+        public bool debugBuild = true;
 
         /// <summary>
         /// If static lighting should be converted to non-static lighting. Only works if debugBuild is true
         /// </summary>
-        public bool debugConvertStaticLightingToNonStatic = false;
+        public readonly bool debugConvertStaticLightingToNonStatic = false;
 
         /// <summary>
         /// If each actor porting should also import into a new asset package that can speed up build 
@@ -89,7 +91,7 @@ namespace CrossGenV.Classes
         /// <summary>
         /// If packages should be resynthesized for cleanliness at the end
         /// </summary>
-        public bool resynthesizePackages = false;
+        public bool resynthesizePackages = true;
 
         /// <summary>
         /// If we should multithread level build
